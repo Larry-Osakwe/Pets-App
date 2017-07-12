48750cf80366db0e532ddbdc777ba25af80bfcc6
Larry <larryosak@gmail.com>
2017-07-11 18:00:34 -0700

    Implement PetCursorAdapterClass

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -6,11 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract.PetEntry;
 
 /**
  * Created by Larry Osakwe on 7/11/2017.
  */
 
+/**
+ * {@link PetCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of pet data as its data source. This adapter knows
+ * how to create list items for each row of pet data in the {@link Cursor}.
+ */
 public class PetCursorAdapter extends CursorAdapter{
 
     public PetCursorAdapter(Context context, Cursor c) {
@@ -20,11 +28,20 @@ public class PetCursorAdapter extends CursorAdapter{
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         // Create and return new blank list item
-        return LayoutInflater.from(context).inflate(R.layout.item_pet, parent, false);
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // Populate list item view with pet data
+
+        TextView petName = (TextView) view.findViewById(R.id.name);
+        TextView petDescription = (TextView) view.findViewById(R.id.summary);
+
+        String name = cursor.getString(cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME));
+        String breed = cursor.getString(cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED));
+
+        petName.setText(name);
+        petDescription.setText(breed);
     }
 }
